3c7927f3bef9645c546e2b515e34d2b60e4a2e43
Arjan ten Hoeve <arjan.ten.hoeve@achmea.nl>
2020-12-04 21:59:46 +0100

    dag1 beide puzzels

--- aoc1/Program.cs
+++ aoc1/Program.cs
@@ -9,21 +9,39 @@ namespace aoc1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
             List<int> numbers = ParseNumberFile("C://Repos//aoc1//aoc1//aoc1//input.txt").ToList();
 
             int rest;
             foreach (int number in numbers)
             {
-                Console.WriteLine(number);
+                //Console.WriteLine(number);
                 rest = 2020 - number;
                 if (numbers.Contains(rest))
                 {
-                    Console.WriteLine($"{number} * {rest} = {number * rest}");
-                    Console.WriteLine();
+                    // 
+                    Console.WriteLine($"som de twee getallen is 2020: {number} * {rest} = {number * rest}");
                 }
             }
 
+            foreach (int number1 in numbers)
+            {
+                foreach(int number2 in numbers)
+                    {
+                    if (numbers.IndexOf(number1)!=numbers.IndexOf(number2))  //verschillend nummer
+                    {
+                        rest = 2020 - number1 - number2;
+                        if (numbers.Contains(rest))
+                        {
+                            Console.WriteLine($"som van de drie getallen is 2020: {number1} * {number2} * {rest} = {number1 * number2 * rest}");
+                            Console.WriteLine();
+                        }
+                        //Console.WriteLine(number1 + number2);
+                    }
+                    
+
+                    } 
+            }
 
 
 
